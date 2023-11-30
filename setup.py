9f9d97aabe7d50c1ccf7d88813324228b0ebc14d
holyvieri <holyvieri@icloud.com>
2023-11-30 11:23:54 -0300

    comentario

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ThinkLearnAI is an open-ended exploration into the fascinating realm of artificial intelligence as part of the course led by Professor Eraylson Galdino at UPE. This dynamic project serves as a hands-on playground for applying concepts learned in the course.',
+    author='Ana_Michael_Rony',
+    license='MIT',
+)
